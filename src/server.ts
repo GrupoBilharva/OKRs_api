76b0379cfde3eee38afa7bc15667666571803043
FilipeSMaciel <filipe10sm@gmail.com>
2026-06-01 14:46:56 -0300

    feat: adicionar cabeçalho de segurança 'X-Content-Type-Options' e 'Referrer-Policy' no middleware

--- src/server.ts
+++ src/server.ts
@@ -31,6 +31,7 @@ app.use(cors({
 
 // Headers de segurança
 app.use((_req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store');
   res.setHeader('X-Frame-Options', 'DENY');
   res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
